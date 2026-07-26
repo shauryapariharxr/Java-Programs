ac714582c582e7d3b4da841353166a95e2dac2c5
Shaurya Parihar <shauryapariharff@gmail.com>
2026-07-26 12:16:11 +0530

    new line

--- test.java
+++ test.java
@@ -1,6 +1,15 @@
 
+import java.util.Scanner;
+
+
 public class test{
     public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+
+      int a;
+      int b;
+
+      a = scanner.nextInt();
 
         System.out.println("test");
 
